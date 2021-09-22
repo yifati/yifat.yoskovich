2c52198c2dc9019ab14aad4caab4b5fdd9ade147
yifati <88625458+yifati@users.noreply.github.com>
2021-09-22 14:10:35 +0300

    Update myFirstServer.js

--- myFirstServer.js
+++ myFirstServer.js
@@ -1,6 +1,32 @@
-var http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const sql = require("./db.js");
 
-http.createServer(function(req,res) {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.end('hello world');
-}).listen(8080);
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true
+}));
+
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to web course example application." });
+});
+
+// getting all customers
+app.get("/customers", function(req, res){
+    sql.query("SELECT * FROM customers", (err, mysqlres) => {
+    if (err) {
+    console.log("error: ", err);
+    res.status(400).send({message: "error in getting all customers: " + err});
+    return;
+    }
+    console.log("got all customers...");
+    res.send(mysqlres);
+    return;
+    });
+    });
+
+app.listen(3000, () => {
+console.log("Server is running on port 3000."
+);
+});
